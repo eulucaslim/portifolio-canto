0b401dd1377de046a69dc5f3fbd9977f690dc256
Lucas Lima <lucaslimacnt14@gmail.com>
2026-06-02 23:07:57 -0400

    fix: add AboutMe component

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,8 +1,10 @@
+
 export default function AboutMe(){
     return (
-        <section>
-            <h2>Sobre Mim</h2>
-            <p>
+        <section className="h-screen flex flex-col">
+            <h2 className="text-8xl ml-10">Sobre Mim</h2>
+            {/* FIXME: Update width! */}
+            <p className="text-2xl m-10 w-96">
                 Graduando em Engenharia de Software pela FUCAPI com foco em Desenvolvimento Full Stack com as linguagens principais
                 como Python, Java e JavaScript/TypeScript, criando soluções escaláveis e com boas práticas de desenvolvimento. 
             </p>
--- app/components/MainContent.tsx
+++ app/components/MainContent.tsx
@@ -3,25 +3,27 @@ import AboutMe from './AboutMe'
 
 export default function MainContent(){
     return (
-        <main className="h-screen flex flex-row justify-around">
-            <section className='flex flex-col justify-center'>
-                <h1>Olá, me chamo Lucas Lima</h1>
-                <h1 className='text-7xl'>
-                    Software 
-                    <br/> 
-                    Developer
-                </h1>
-            </section>
-            <aside className='flex flex-col justify-center'>
-                <Image
-                    src="/profile.jpeg"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                    className='rounded-s-full'
-                />
-            </aside>
-            {/* <AboutMe/> */}
+        <main>
+            <article className='h-screen flex flex-row justify-around'>
+                <section className='flex flex-col justify-center'>
+                    <h1>Olá, me chamo Lucas Lima</h1>
+                    <h1 className='text-7xl'>
+                        Software 
+                        <br/> 
+                        Developer
+                    </h1>
+                </section>
+                <aside className='flex flex-col justify-center'>
+                    <Image
+                        src="/profile.jpeg"
+                        width={500}
+                        height={500}
+                        alt="Picture of the author"
+                        className='rounded-s-full'
+                    />
+                </aside>
+            </article>
+            <AboutMe/>
         </main>
     )
 }
